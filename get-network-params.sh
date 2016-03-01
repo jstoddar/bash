bfc8b99dd3bf30a106ee578c8e04295088d1c2cc
John Stoddard <john.stoddard@oracle.com>
2016-03-01 14:21:07 -0800

    chmod 755 cider-to-netmask.sh get-fc-info.sh get-network-params.sh update get-network-params.sh to set host_name variable and do basic checking.

--- get-network-params.sh
+++ get-network-params.sh
@@ -3,6 +3,22 @@
 # Bash script/lib to get current networking parameters.
 # Works on rpm based distros versions 6, 7 and debian 14 LTS.
 # Requires additional bash lib: cider-to-netmask.sh
+# To debug or test use -v or --v as first parameter
+
+testPrintOut(){
+# Script sets the following variables listed below.
+if [ "X$1" == 'X-v' ] || [ "X$1" == 'X--v' ]; then
+  echo "default_route       $default_route
+public_interface    $public_interface
+net_work            $net_work
+public_link_status  $public_link_status
+hw_addr             $hw_addr
+public_ip           $public_ip
+cidr_netmask        $cidr_netmask
+net_mask            $net_mask
+host_name           $host_name"
+fi
+}
 
 sourceCidrToNetmaskLib(){
 # cider-to-netmask.sh uses getDottedQuadNetmaskFromCidr
@@ -36,24 +52,21 @@ eval $(ip route list src $public_ip| \
     print "net_work="$1}')
 }
 
-testPrintOut(){
-# Sets the following variables listed below.
-# To debug or test use the -v switch
-if [ "X$1" == 'X-v' ] || [ "X$1" == 'X--v' ]; then
-  echo "default_route       $default_route
-public_interface    $public_interface
-net_work            $net_work
-public_link_status  $public_link_status
-hw_addr             $hw_addr
-public_ip           $public_ip
-cidr_netmask        $cidr_netmask
-net_mask            $net_mask"
-fi
+getCheckFullyQualifiedHostname(){
+# Check to make sure that the hostame obtained appears
+#  fully qualified... function is also handy for checking user input.
+host_name=$(hostname)
+if [ "$(echo $host_name| awk -F. '{print NF}')" -lt 2 ] ; then
+   echo "hostname appears to be not fully qualifed...
+ Aborting."
+exit 1
+fi 
 }
 
 ### Main
 getNetworkParams
 sourceCidrToNetmaskLib
 getDottedQuadNetmaskFromCidr $cidr_netmask
+getCheckFullyQualifiedHostname
 testPrintOut $*
 
